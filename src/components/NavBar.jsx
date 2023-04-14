44dd00f0ba642131194ad9dc7366a8fedced9f7a
pyeong777 <sdp1123@naver.com>
2023-04-14 14:29:16 +0900

    Rename: change Header.jsx -> NavBar.jsx

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import { BsFillPencilFill } from "react-icons/bs";
+import { FiShoppingBag } from "react-icons/fi";
+
+export default function NavBar() {
+  return (
+    <header className="flex p-4">
+      <Link to="/" className="flex justify-center">
+        <FiShoppingBag />
+        <h1>Shoppy</h1>
+      </Link>
+      <Link to="/products">
+        <p>Products</p>
+      </Link>
+      <Link to="/carts">
+        <p>Carts</p>
+      </Link>
+      <Link to="/products/new">
+        <BsFillPencilFill />
+      </Link>
+      <Link>
+        <p>Login</p>
+      </Link>
+    </header>
+  );
+}
