130fcd5ecdf0463e7fc5b4066433f3f89c80b867
pyeong777 <sdp1123@naver.com>
2023-04-14 14:31:37 +0900

    feat: add Outlet

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import NavBar from "./components/NavBar";
+
+const queryClient = new QueryClient();
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+      </QueryClientProvider>
+    </>
+  );
 }
 
 export default App;
