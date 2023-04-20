b5123eb42db0631e785dbaf146f509b32d4a80d5
pyeong777 <sdp1123@naver.com>
2023-04-20 22:39:05 +0900

    feat: add AuthContextProvider

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NavBar from "./components/NavBar";
+import { AuthContextProvider } from "./components/context/AuthContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <NavBar />
-      <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <NavBar />
         <Outlet />
-      </QueryClientProvider>
+      </AuthContextProvider>
     </>
   );
 }
