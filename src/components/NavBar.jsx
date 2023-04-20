c03728df798a48b4ccbfd3b7da0240954538973a
pyeong777 <sdp1123@naver.com>
2023-04-20 22:42:55 +0900

    refactor: use context

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,36 @@
 import { Link } from "react-router-dom";
 import { BsFillPencilFill } from "react-icons/bs";
 import { FiShoppingBag } from "react-icons/fi";
-import { login, logout, onUserStateChange } from "./../api/firebase";
 import { useEffect, useState } from "react";
+import User from "./User";
+import Button from "./ui/Button";
+import { useAuthContext } from "./context/AuthContext";
 
 export default function NavBar() {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    onUserStateChange(setUser);
-  }, []);
+  const { user, login, logout } = useAuthContext();
 
   return (
-    <header className="flex justify-between py-2 border-b border-gray-300">
+    <header className="flex justify-between w-full py-2 border-b border-gray-300">
       <Link to="/" className="flex items-center ml-4 text-3xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav className="flex items-center gap-4 mr-4 font-semibold">
+      <nav className="flex items-center gap-4 mr-4 font-semibold ">
         <Link to="/products">
           <p>Products</p>
         </Link>
         <Link to="/carts">
           <p>Carts</p>
         </Link>
-        <Link to="/products/new">
-          <BsFillPencilFill />
-        </Link>
-        {!user && <button onClick={login}>Login</button>}
-        {user && <button onClick={logout}>Logout</button>}
+        {user && user.isAdmin && (
+          <Link to="/products/new">
+            <BsFillPencilFill />
+          </Link>
+        )}
+
+        {user && <User user={user} />}
+        {!user && <Button text={"Login"} onClick={login} />}
+        {user && <Button text={"Logout"} onClick={logout} />}
       </nav>
     </header>
   );
