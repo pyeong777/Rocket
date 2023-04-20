512cba56f2875a192d37e1de81dcabc246d0550d
pyeong777 <sdp1123@naver.com>
2023-04-21 03:09:32 +0900

    feat: add ProtectedRoute.jsx (check user & admin)

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+import { useAuthContext } from "../components/context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children, requireAdmin }) {
+  const { user } = useAuthContext();
+
+  //유저정보가 없거나 어드민 확인 요청을 했는데 어드민이 아닌경우
+  if (!user || (requireAdmin && !user.isAdmin)) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+}
