6546234ef45ffdc6f74257f877e76920240ed9c7
pyeong777 <sdp1123@naver.com>
2023-05-02 19:04:13 +0900

    design: image hover scale

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,7 +3,7 @@ export default function ProductCard({
 }) {
   const won = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return (
-    <li className="overflow-hidden rounded-lg shadow-md cursor-pointer">
+    <li className="overflow-hidden rounded-lg shadow-md cursor-pointer hover:scale-105">
       <img className="w-full" src={image} alt={title} />
       <div className="flex items-center justify-between px-2 mt-2 text-base">
         <h3 className="truncate">{title}</h3>
