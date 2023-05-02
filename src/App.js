1baae7153d5839de313209206360ecdc77381f29
pyeong777 <sdp1123@naver.com>
2023-05-03 01:45:49 +0900

    fix: import directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NavBar from "./components/NavBar";
-import { AuthContextProvider } from "./components/context/AuthContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const queryClient = new QueryClient();
 
