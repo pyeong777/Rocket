d9b8c5fea544cf28a3075977797f7365eaff4530
pyeong777 <sdp1123@naver.com>
2023-05-03 13:53:34 +0900

    feat: add staleTime at getProducts function

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,7 +7,7 @@ export default function Products() {
     isLoading,
     error,
     data: products,
-  } = useQuery(["products"], () => getProducts());
+  } = useQuery(["products"], () => getProducts(), { statleTime: 1000 * 60 });
   return (
     <>
       {isLoading && <p>Loading...</p>}
