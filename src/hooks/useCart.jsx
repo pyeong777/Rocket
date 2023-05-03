e17bfd94b83c907625819985222fb3f5a0d8bcdb
pyeong777 <sdp1123@naver.com>
2023-05-04 00:58:52 +0900

    feat: add useCart custom hooks

---
+++ src/hooks/useCart.jsx
@@ -0,0 +1,29 @@
+import { addOrUpdateToCart, getCart, removeFromCart } from "../api/firebase";
+import { useAuthContext } from "../context/AuthContext";
+import { useQueryClient, useQuery, useMutation } from "@tanstack/react-query";
+
+export default function useCart() {
+  const { uid } = useAuthContext();
+  const queryClient = useQueryClient();
+
+  const cartQuery = useQuery(["carts", uid || ""], () => getCart(uid), {
+    enabled: !!uid,
+  });
+
+  const addOrUpdateItem = useMutation(
+    (product) => addOrUpdateToCart(uid, product),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["carts", uid]);
+      },
+    }
+  );
+
+  const removeItem = useMutation((id) => removeFromCart(uid, id), {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["carts", uid]);
+    },
+  });
+
+  return { cartQuery, addOrUpdateItem, removeItem };
+}
