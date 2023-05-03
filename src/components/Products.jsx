7e0a63bbb440f48a8cac02709b768e135cdbecab
pyeong777 <sdp1123@naver.com>
2023-05-03 16:51:34 +0900

    refactor: make react-query custom hooks

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,11 @@
-import { getProducts } from "../api/firebase";
-import { useQuery } from "@tanstack/react-query";
+import useProducts from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
 
 export default function Products() {
   const {
-    isLoading,
-    error,
-    data: products,
-  } = useQuery(["products"], () => getProducts(), { statleTime: 1000 * 60 });
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
+
   return (
     <>
       {isLoading && <p>Loading...</p>}
