0e819da7f64d27819280eae764523110c6c9d607
pyeong777 <sdp1123@naver.com>
2023-05-06 01:39:57 +0900

    -

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { BsFillPencilFill } from "react-icons/bs";
 import { FiShoppingBag } from "react-icons/fi";
+import { TfiThemifyFaviconAlt } from "react-icons/tfi";
 import User from "./User";
 import Button from "./ui/Button";
 import { useAuthContext } from "../context/AuthContext";
@@ -12,8 +13,8 @@ export default function NavBar() {
   return (
     <header className="flex justify-between w-full py-2 border-b border-gray-300">
       <Link to="/" className="flex items-center ml-4 text-3xl text-brand">
-        <FiShoppingBag />
-        <h1>Shoppy</h1>
+        <TfiThemifyFaviconAlt />
+        <h1 className="px-2 font-bold">Rocket</h1>
       </Link>
       <nav className="flex items-center gap-4 mr-4 font-semibold ">
         <Link to="/products">
